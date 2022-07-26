3742de1fe8ad48d25c7d997c33ed24eda31d7687
kobby08 <linusquam@gmail.com>
2022-07-26 20:42:25 +0000

    useEffect cleanup, aborting a fetch request

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,6 @@
 // const Bloglist = (props.blogs, props.title) => {}
 
-const BlogList = ({ blogs, title, handleDelete }) => {
+const BlogList = ({ blogs, title }) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
@@ -8,13 +8,6 @@ const BlogList = ({ blogs, title, handleDelete }) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by: {blog.author}</p>
-          <button
-            onClick={() => {
-              handleDelete(blog.id);
-            }}
-          >
-            Delete
-          </button>
         </div>
       ))}
     </div>
