0096d144d7f86a1fee7413404e0e7681092b1c13
kobby08 <linusquam@gmail.com>
2022-07-26 19:55:38 +0000

    using router link component

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,16 @@
+// in order to allow React to handle navigation and not
+// request templates from the server every time.
+// we use the Link component from the react-router to achieve this.
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>Nozo Blog</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a
-          href="/create"
+        <Link to="/">Home</Link>
+        <Link
+          to="/create"
           style={{
             color: "white",
             backgroundColor: "#f1356d",
@@ -13,7 +18,7 @@ const Navbar = () => {
           }}
         >
           New Blog
-        </a>
+        </Link>
       </div>
     </nav>
   );
