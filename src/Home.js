86bdb79318efc90c371aee867192c1e9b58b6c52
kobby08 <linusquam@gmail.com>
2022-07-26 16:21:35 +0000

    custom hooks

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,13 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  // we can allow useEffect to listen to changes in dependencies before it run.
-  // useEffect will not run until a dependenciy changes.
-  // dependencies are passed as array to the useEffect hook.
-  useEffect(() => {
-    fetch("http://localhost:8000/blogss")
-      .then((res) => {
-        if (!res.ok) {
-          throw Error("could not fetch data..");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        setIsPending(false);
-        setError(null);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setIsPending(false);
-      });
-  }, []);
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
