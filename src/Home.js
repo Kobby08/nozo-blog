833ea34fce18be7e3926e3cc3d2edaee899a4e36
kobby08 <linusquam@gmail.com>
2022-07-26 16:07:16 +0000

    handling fetch error

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,35 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   // we can allow useEffect to listen to changes in dependencies before it run.
   // useEffect will not run until a dependenciy changes.
   // dependencies are passed as array to the useEffect hook.
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => res.json())
+    fetch("http://localhost:8000/blogss")
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("could not fetch data..");
+        }
+        return res.json();
+      })
       .then((data) => {
         setBlogs(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsPending(false);
       });
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
