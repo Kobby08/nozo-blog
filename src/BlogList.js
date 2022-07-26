5e258dfd8b9e11a139031ade91ec66095a3ef3b0
kobby08 <linusquam@gmail.com>
2022-07-26 14:18:53 +0000

    passing functions as props

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,10 +1,24 @@
-const BlogList = ({ blogs }) => {
-  return blogs.map((blog) => (
-    <div className="blog-preview" key={blog.id}>
-      <h2>{blog.title}</h2>
-      <p>Written by: {blog.author}</p>
+// const Bloglist = (props.blogs, props.title) => {}
+
+const BlogList = ({ blogs, title, handleDelete }) => {
+  return (
+    <div className="blog-list">
+      <h2>{title}</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by: {blog.author}</p>
+          <button
+            onClick={() => {
+              handleDelete(blog.id);
+            }}
+          >
+            Delete
+          </button>
+        </div>
+      ))}
     </div>
-  ));
+  );
 };
 
 export default BlogList;
